0a60f11a3f05cdeec3d7c68f6f9391437d87c7d7
Abel <se.abel.abate@gmail.com>
2021-01-27 20:27:14 +0300

    withdrawal added

--- Bank/assets/js/app.js
+++ Bank/assets/js/app.js
@@ -20,3 +20,21 @@ function Transfer(amount,total){
 
 
 }
+
+
+
+
+function withdrawal(amount,total) {
+    if (total<50){
+        console.log(" Access denied you have reached the minimum for withdawal request")
+    }
+    else if (amount>total){
+        console.log("Access denied insufficent funds.")
+    }
+    else if (amount>10000){
+        console.log("Maximum withdraw limit reached.");
+    }
+    else if(total>=50 | amount<=total){
+          console.log("Access granted your remanin balance is " + (total - amount));
+    }
+}
