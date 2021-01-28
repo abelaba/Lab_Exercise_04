b9e002ef5f5651b5b6a9a4f6720552eb2d167513
Abel <se.abel.abate@gmail.com>
2021-01-28 12:20:48 +0300

    Bank system changed to object

--- Bank/assets/js/app.js
+++ Bank/assets/js/app.js
@@ -1,65 +1,93 @@
 
+let Bank = {
+  total:600,
+  deposit:function(amount) {
+    this.total = this.total + amount
+      console.log("Success. Your balance is "+ (this.total));
+      leavstay()
 
-function Deposit(amount,total) {
-    console.log("Success. Your balance is "+ (total + amount));
+  },
+  balance:function () {
+      console.log("Your balance is " + this.total);
+      leavstay()
+
+  },
+  transfer:function(amount){
+      let x = (this.total-amount);
+      if(x > 0){
+        this.total = this.total - amount;
+          console.log("Transfer successful. Your remanining balance is "+ x);
+      }
+      else{
+          console.log("Insufficent Balance.");
+      }
+      leavstay()
+
+
+  },
+  withdrawal:function(amount,total) {
+      if (this.total<50){
+          console.log(" Access denied you have reached the minimum for withdawal request")
+      }
+      else if (amount>this.total){
+          console.log("Access denied insufficent funds.")
+      }
+      else if (amount>10000){
+          console.log("Maximum withdraw limit reached.");
+      }
+      else if(this.total>=50 | amount<=this.total){
+        this.total = this.total - amount
+
+
+            console.log("Access granted your remanin balance is " + (this.total));
+      }
+      leavstay()
+
+  }
+
+
 
 }
-function balance(total) {
-    console.log("Your balance is " + total);
+
+
+function leavstay() {
+  var question = prompt("Do you want to leave or stay?\n 1 = leave\n 2 = Stay ")
+  if (question == "1") {
+
+    console.log("End.");
+
+  }else if(question == "2") {
+    main()
+
+  }
 
 }
 
-function Transfer(amount,total){
-    let x = (total-amount);
-    if(x > 0){
-        console.log("Transfer successful. Your remanining balance is "+ x);
-    }
-    else{
-        console.log("Insufficent Balance.");
-    }
-
-
-}
-
-
-
-
-function withdrawal(amount,total) {
-    if (total<50){
-        console.log(" Access denied you have reached the minimum for withdawal request")
-    }
-    else if (amount>total){
-        console.log("Access denied insufficent funds.")
-    }
-    else if (amount>10000){
-        console.log("Maximum withdraw limit reached.");
-    }
-    else if(total>=50 | amount<=total){
-          console.log("Access granted your remanin balance is " + (total - amount));
-    }
-}
-
-
-
-
-(function () {
+function main() {
     let x = eval(prompt("Choose\n 1 = Deposit \n 2 = Withdrawal \n 3 = Transfer \n 4 = Balance" ));
 
-    var total = 600;
+
     if (x==1 | x==2 |x==3 |x==5){
         let amount = eval(prompt("Enter the Amount"));
         if (x == 1) {
-            Deposit(amount,total);
+            Bank.deposit(amount);
                     }
         else if (x == 2) {
-            withdrawal(amount,total);
+            Bank.withdrawal(amount);
             }
         else if (x == 3) {
-            Transfer(amount,total);
+            Bank.transfer(amount);
         }
 
     }
     else if(x==4){
-        balance(total);
+        Bank.balance();
     }
+}
+
+
+
+
+(function(){
+  main()
 })()
